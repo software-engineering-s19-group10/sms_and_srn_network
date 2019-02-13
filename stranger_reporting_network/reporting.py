423caa6060e492a60b067f1fb9b3827c0c7166a2
mnadev <mohammednadeem902@gmail.com>
2019-02-12 22:35:58 -0500

    added basic functionality to add to db and get local areas from it

---
+++ stranger_reporting_network/reporting.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+from haversine import haversine
+
+db_filename = 'database/srn.db'
+
+global db_conn
+global cursor
+
+def init_db():
+    global db_conn
+    db_conn = sqlite3.connect(db_filename)
+    global cursor
+    cursor = db_conn.cursor()
+    cursor.execute('''CREATE TABLE SRN
+             (REAL latitude, REAL longitude, INT severity);''')
+    cursor.execute('''USE database_name;''')
+
+
+
+# function to add stranger to database
+def report_stranger(lat, long, severity):
+    global cursor
+    cursor.execute("INSERT INTO SRN (?,?,?);", lat, long, severity)
+    return
+
+def send_closest_srn(lat_user, long_user):
+    global cursor
+    table = cursor.fetchall()
+    return_array = []
+    for row in table:
+        if haversine((row[0],row[1]), (lat_user, long_user), unit="mi") < 5:
+            return_array.append({"lat": row[0], "long": row[1]})
+
+    return return_array
+
